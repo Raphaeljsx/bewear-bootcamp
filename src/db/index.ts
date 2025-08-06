a147f7880cc30d457a4823b60e8fb98975499834
RaphaelJsx <raphaelalvoliv@gmail.com>
2025-08-06 00:31:50 -0300

    chore: add better auth tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,8 @@
 import "dotenv/config";
 
+import * as schema from "./schema";
 import { drizzle } from "drizzle-orm/node-postgres";
-export const db = drizzle(process.env.DATABASE_URL!);
+
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+});
